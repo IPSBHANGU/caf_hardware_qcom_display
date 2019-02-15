5fcf502af04adaacada08b25f354b7910192281f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-02-15 11:46:11 +0530

    display: Get rid of useless makefile guard
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- Android.mk
+++ Android.mk
@@ -5,11 +5,6 @@ display-hals += libmemtrack
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
 display-hals += liblight
 endif
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-    include $(call all-named-subdir-makefiles,$(display-hals))
-else
-ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
-    include $(call all-named-subdir-makefiles,$(display-hals))
-endif
-endif
+
+include $(call all-named-subdir-makefiles,$(display-hals))
 endif
