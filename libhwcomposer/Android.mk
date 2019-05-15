5e60e85e73a9720d772efc0750a399575f4310df
Mohd Faraz <mohd.faraz.abc@gmail.com>
2019-05-15 09:55:53 +0000

    display: Fixed Compilation
    Change-Id: Icaf9b088d8015894cc6e1e267641853811e15f40
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- libhwcomposer/Android.mk
+++ libhwcomposer/Android.mk
@@ -1,4 +1,7 @@
-LOCAL_PATH := $(call my-dir) include $(LOCAL_PATH)/../common.mk include $(CLEAR_VARS)
+LOCAL_PATH := $(call my-dir)
+
+include $(LOCAL_PATH)/../common.mk
+include $(CLEAR_VARS)
 
 LOCAL_MODULE                  := hwcomposer.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_RELATIVE_PATH    := hw
