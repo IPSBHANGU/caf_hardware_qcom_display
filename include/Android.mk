342ae8456c06fa3a4e1575ebeb6b86acfad4a8c5
Naseer Ahmed <naseer@codeaurora.org>
2019-05-20 13:07:05 +0530

    display: Partially convert makefiles to blueprints
     [@Androiabledroid]: Port for it
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- include/Android.mk
+++ include/Android.mk
@@ -2,20 +2,23 @@ LOCAL_PATH:= $(call my-dir)
 include $(LOCAL_PATH)/../common.mk
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		      := color_metadata_headers
-LOCAL_EXPORT_C_INCLUDE_DIRS   := $(LOCAL_PATH)
-include $(BUILD_HEADER_LIBRARY)
-
-include $(CLEAR_VARS)
 #TODO move all exported headers to this directory
-LOCAL_MODULE := display_headers
-LOCAL_EXPORT_C_INCLUDE_DIRS   := $(display_top)/libcopybit \
-                                 $(display_top)/libqdutils \
-                                 $(display_top)/libqservice
-
-ifeq ($(TARGET_USES_GRALLOC1), true)
-    LOCAL_EXPORT_C_INCLUDE_DIRS += $(display_top)/libgralloc1
-else
-    LOCAL_EXPORT_C_INCLUDE_DIRS += $(display_top)/libgralloc
-endif
-include $(BUILD_HEADER_LIBRARY)
+#LOCAL_MODULE := display_headers
+LOCAL_VENDOR_MODULE           := true
+LOCAL_COPY_HEADERS_TO         := $(common_header_export_path)
+LOCAL_COPY_HEADERS	      := color_metadata.h \
+				 ../libqdutils/cb_swap_rect.h \
+				 ../libqdutils/cb_utils.h \
+				 ../libqdutils/comptype.h \
+				 ../libqdutils/display_config.h \
+				 ../libqdutils/DisplayConfig.h \
+				 ../libqdutils/idle_invalidator.h \
+				 ../libqdutils/mdp_version.h \
+				 ../libqdutils/profiler.h \
+				 ../libqdutils/qdMetaData.h \
+				 ../libqdutils/qd_utils.h \
+                                 ../libqservice/IQClient.h \
+				 ../libqservice/IQService.h \
+				 ../libqservice/QService.h \
+				 ../libqservice/QServiceUtils.h
+include $(BUILD_COPY_HEADERS)
